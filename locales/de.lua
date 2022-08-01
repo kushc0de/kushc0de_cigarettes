b1e9607d919909317700cf67619b3630a7abc6fc
kushc0de <69555828+kushc0de@users.noreply.github.com>
2022-08-01 14:03:07 +0200

    added okok Support in locales

--- locales/de.lua
+++ locales/de.lua
@@ -7,5 +7,7 @@ Locales['de'] = {
     ['not_enough_space'] = 'Du hast nicht genügend Platz in deinem Inventar.',
     ['not_enough_money'] = 'Du hast nicht genügend Bargeld dabei.',
     ['success'] = 'Du hast erfolgreich für %s eingekauft',
-    ['helpnotify'] = 'Drücke ~INPUT_CONTEXT~ um Zigaretten zu kaufen'
+    ['helpnotify'] = 'Drücke ~INPUT_CONTEXT~ um Zigaretten zu kaufen',
+    ['notify_title'] = 'Zigarettenautomat',
+    ['info_textui'] = 'Drücke [E] um Zigaretten zu kaufen'
   }
--- locales/en.lua
+++ locales/en.lua
@@ -7,5 +7,7 @@ Locales['en'] = {
     ['not_enough_space'] = 'You dont have enough space in your inventory.',
     ['not_enough_money'] = 'You dont have enough money in your pocket',
     ['success'] = 'You have successfully shopped for %s',
-    ['helpnotify'] = 'Press ~INPUT_CONTEXT~ to buy cigarettes'
+    ['helpnotify'] = 'Press ~INPUT_CONTEXT~ to buy cigarettes',
+    ['notify_title'] = 'Cigarette Vending Machine',
+    ['info_textui'] = 'Press [E] to buy cigarettes'
   }
